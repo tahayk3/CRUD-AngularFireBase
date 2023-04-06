d709dad32607c66a29edbc2daafdb777ab4fde79
tahayk3 <tahayk3@gmail.com>
2023-04-06 16:01:02 -0600

    insert de empleado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,29 @@ import { Empleado } from './empleado.model';
 })
 export class AppComponent {
   titulo = 'Listado de empleados';
+  cuadroNombre:string = "";
+  cuadroApellido:string = "";
+  cuadroCargo:string = "";
+  cuadroSalario:number = 0;
+
   empleados: Empleado[] =[
     new Empleado("Juan", "Diaz", "Presidente", 7500),
     new Empleado("Maria", "Martin", "Presidente", 9500),
-    new Empleado("Pedro", "Fernandes", "Presidente", 2500)
+    new Empleado("Pedro", "Fernandes", "Presidente", 2500),
+    new Empleado("Guillermo ", "Fernandes", "Presidente", 2500),
+    new Empleado("Anibal", "Fernandes", "Presidente", 2500),
+    new Empleado("Pedro", "Fernandes", "Presidente", 26500),
+    new Empleado("Pedro", "Fernandes", "Presidente", 5500),
+    new Empleado("Pedro", "Fernandes", "Presidente", 12500),
+    new Empleado("Pedro", "Fernandes", "Presidente", 7500),
+    new Empleado("Pedro", "Fernandes", "Presidente", 8500),
+    new Empleado("Pedro", "Fernandes", "Presidente", 9500),
+    new Empleado("Pedro", "Fernandes", "Presidente", 2500),
+    new Empleado("Pedro", "Fernandes", "Presidente", 45500),
+    new Empleado("Pedro", "Fernandes", "Presidente", 7500)
   ];
+  agregarEmpleado(){
+    let newEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    this.empleados.push(newEmpleado);
+  }
 }
