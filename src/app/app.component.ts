b144831a8d65af73d8139e305f9c62f5add3c4ee
tahayk3 <tahayk3@gmail.com>
2023-04-06 17:36:11 -0600

    paginacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Empleado } from './empleado.model';
 
 @Component({
@@ -6,12 +6,13 @@ import { Empleado } from './empleado.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   titulo = 'Listado de empleados';
   cuadroNombre:string = "";
   cuadroApellido:string = "";
   cuadroCargo:string = "";
   cuadroSalario:number = 0;
+  config:any;
 
   empleados: Empleado[] =[
     new Empleado("Juan", "Diaz", "Presidente", 7500),
@@ -19,16 +20,26 @@ export class AppComponent {
     new Empleado("Pedro", "Fernandes", "Presidente", 2500),
     new Empleado("Guillermo ", "Fernandes", "Presidente", 2500),
     new Empleado("Anibal", "Fernandes", "Presidente", 2500),
-    new Empleado("Pedro", "Fernandes", "Presidente", 26500),
-    new Empleado("Pedro", "Fernandes", "Presidente", 5500),
-    new Empleado("Pedro", "Fernandes", "Presidente", 12500),
-    new Empleado("Pedro", "Fernandes", "Presidente", 7500),
-    new Empleado("Pedro", "Fernandes", "Presidente", 8500),
-    new Empleado("Pedro", "Fernandes", "Presidente", 9500),
-    new Empleado("Pedro", "Fernandes", "Presidente", 2500),
-    new Empleado("Pedro", "Fernandes", "Presidente", 45500),
-    new Empleado("Pedro", "Fernandes", "Presidente", 7500)
+    new Empleado("Nissa", "Fernandes", "Presidente", 26500),
+    new Empleado("Grekko", "Fernandes", "Presidente", 5500),
+    new Empleado("Io", "Fernandes", "Presidente", 12500),
+    new Empleado("Karen", "Fernandes", "Presidente", 7500),
+    new Empleado("Valeria", "Fernandes", "Presidente", 8500)
   ];
+
+  ngOnInit(): void {
+
+    this.config = {
+      itemsPerPage: 5,
+      currentPage: 1,
+      totalItems: 0
+    };
+  }
+  
+  pageChange(event: any){
+    this.config.currentPage = event;
+  }
+
   agregarEmpleado(){
     let newEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
     this.empleados.push(newEmpleado);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
